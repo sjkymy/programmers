662ec0c9975bd744cd89f36ca00cc8bc853459c1
SeongJun KIM <112460273+sjkymy@users.noreply.github.com>
2023-03-26 00:09:20 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
+++ 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -1,9 +1,7 @@
 function solution(n) {
-    let answer = [];
     for (let x = 0; x < n; x++ ) {
         if (n % x === 1) {
-            answer.push(x)
+            return x
         }
     }
-    return answer[0];
 }
