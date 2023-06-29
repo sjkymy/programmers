cabdf19ac8d87ad6d55b1608db5824052d1c7612
SeongJun KIM <112460273+sjkymy@users.noreply.github.com>
2023-06-29 09:37:51 +0900

    [level 1] Title: 내적, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,11 +1,7 @@
 function solution(a, b) {
     let answer = 0;
     for (let i = 0; i < a.length; i++) {
-        for (let j = 0; j < b.length; j++) {
-            if (i === j) {
-                answer += a[i]*b[j]
-            }
-        }
+        answer += a[i]*b[i]
     }
     return answer;
 }
